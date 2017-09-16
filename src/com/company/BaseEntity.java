173be46f80846ffe4aff74a3930d3805e34df875
Dmitry Popov <dimonrtm@gmail.com>
2017-09-16 12:16:01 +0600

    hjfhhf

--- src/com/company/BaseEntity.java
+++ src/com/company/BaseEntity.java
@@ -4,4 +4,24 @@ package com.company;
  * Created by админъ on 16.09.2017.
  */
 public class BaseEntity {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "BaseEntity{"+"id="+id+"}";
+    }
+
+    public boolean isNew()
+    {
+        return (this.id==0);
+    }
 }
